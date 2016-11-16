a0a46ddea0d79d01b009aab4d1af9e1d45fe9e99
programmingo <chenjunyu@go.thu.edu.tw>
2016-11-17 06:36:06 +0800

    the function of binary search

---
+++ binary_search.cpp
@@ -0,0 +1,19 @@
+int binary_search(int *arr, int N, int find)
+{
+ int left=0;
+ int right=N-1;
+ int mid;
+
+ while (left<=right)
+ {
+  mid=(left+right)/2;
+  if (find < arr[mid] )
+    right=mid-1;
+  else if (find > arr[mid])
+    left=mid+1;
+  else
+    return mid;
+ }
+ printf("Not found.\n");
+ exit(1);
+}
