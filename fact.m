d55b21d5a2b0d32ef412783a760f03b958056202
programmingo <chenjunyu@go.thu.edu.tw>
2016-11-17 09:51:58 +0800

    hw 1.2 main

---
+++ fact.m
@@ -0,0 +1,20 @@
+% Data Structures
+% HW 1.2
+% compute factorial from 0 to 50
+clear all;
+
+n=input("Please input a number: ");
+
+a=1;
+for i=1:n
+  a=a*i;
+  a=carry(a);
+  
+  len=length(a);
+  str='%2d!=';
+  for j=1:len
+    str=[str, '%d'];
+  end
+  str=[str, '\n'];
+  fprintf(str, i, fliplr(a));
+end
